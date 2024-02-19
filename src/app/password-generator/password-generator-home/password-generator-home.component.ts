3d861bbca5546ee97beffa3ae72a56fa47ec68b4
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-19 10:46:47 +0330

    added font awesome and icons

--- src/app/password-generator/password-generator-home/password-generator-home.component.ts
+++ src/app/password-generator/password-generator-home/password-generator-home.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-password-generator-home',
   templateUrl: './password-generator-home.component.html',
-  styleUrls: ['./password-generator-home.component.css']
+  styleUrls: ['./password-generator-home.component.css'],
 })
 export class PasswordGeneratorHomeComponent {
+  isNumbersIncluded: boolean = false;
+  isSymbolsIncluded: boolean = false;
+  islettersIncluded: boolean = false;
+  length: number = 0;
+  password: string = '';
+  constructor() {}
 
+  onPassGeneratorBtnClicked() {
+    const numbers = '0123456789';
+    const letters = 'zxcvbnmasdfghjklqwertyuiop';
+    const symbols = '!@#$%^&*()_+|';
+  }
 }
