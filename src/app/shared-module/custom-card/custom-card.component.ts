6c1e82730b781951563d7365f6bdd96e1178288d
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-21 17:25:24 +0330

    added ng for for cards

--- src/app/shared-module/custom-card/custom-card.component.ts
+++ src/app/shared-module/custom-card/custom-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-custom-card',
   templateUrl: './custom-card.component.html',
-  styleUrls: ['./custom-card.component.css']
+  styleUrls: ['./custom-card.component.css'],
 })
-export class CustomCardComponent {
-
+export class CustomCardComponent implements OnInit {
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() userName = '';
+  @Input() content = '';
+  constructor() {}
+  ngOnInit(): void {
+    console.log('log in custom card');
+  }
 }
