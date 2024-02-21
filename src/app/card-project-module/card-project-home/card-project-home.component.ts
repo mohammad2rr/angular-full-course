f4b3a31c34969718646fe98dcdcfab80f3781cf9
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-21 17:04:56 +0330

    added custom card data

--- src/app/card-project-module/card-project-home/card-project-home.component.ts
+++ src/app/card-project-module/card-project-home/card-project-home.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-project-home',
   templateUrl: './card-project-home.component.html',
-  styleUrls: ['./card-project-home.component.css']
+  styleUrls: ['./card-project-home.component.css'],
 })
 export class CardProjectHomeComponent {
-
+  customCardData = [
+    {
+      title: 'item1',
+      imageUrl: 'https://bulma.io/images/placeholders/96x96.png',
+      userName: 'user1',
+      content: 'user1-content',
+    },
+    {
+      title: 'item2',
+      imageUrl: 'https://bulma.io/images/placeholders/96x96.png',
+      userName: 'user2',
+      content: 'user2-content',
+    },
+    {
+      title: 'item3',
+      imageUrl: 'https://bulma.io/images/placeholders/96x96.png',
+      userName: 'user3',
+      content: 'user3-content',
+    },
+  ];
 }
