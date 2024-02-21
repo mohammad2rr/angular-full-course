245405bef340a3ce4dfb8cb8dcb9c89b5065ddd2
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-21 16:35:33 +0330

    added custom card component to shared module

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CustomCardComponent } from './custom-card/custom-card.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CustomCardComponent],
+  imports: [CommonModule],
+  exports: [CustomCardComponent],
 })
-export class SharedModuleModule { }
+export class SharedModuleModule {}
