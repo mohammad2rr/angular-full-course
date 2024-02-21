dddb1dbf7594d990f4ee5c82f6266de1119ed915
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-21 16:28:01 +0330

    added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app-component/app.component';
 import { PasswordGeneratorModule } from './password-generator-module/password-generator.module';
 import { CardProjectModule } from './card-project-module/card-project.module';
 import { HomeModule } from './home-module/home.module';
+import { SharedModuleModule } from './shared-module/shared-module.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { HomeModule } from './home-module/home.module';
     PasswordGeneratorModule,
     CardProjectModule,
     HomeModule,
+    SharedModuleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
