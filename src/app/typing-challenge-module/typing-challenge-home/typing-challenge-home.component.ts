d1baacfcd28a3ee7eb216b10183fd6e555454e55
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-22 11:24:43 +0330

    added faker for generating random text

--- src/app/typing-challenge-module/typing-challenge-home/typing-challenge-home.component.ts
+++ src/app/typing-challenge-module/typing-challenge-home/typing-challenge-home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-typing-challenge-home',
   templateUrl: './typing-challenge-home.component.html',
-  styleUrls: ['./typing-challenge-home.component.css']
+  styleUrls: ['./typing-challenge-home.component.css'],
 })
 export class TypingChallengeHomeComponent {
-
+  randomText: string = faker.lorem.sentence();
 }
