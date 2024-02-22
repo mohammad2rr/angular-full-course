89fc03ac41ee2902a928dff1d51416f4cc8d42dd
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-22 13:32:54 +0330

    added styles and base structure for pipes section

--- src/app/pipes-module/pipes-home/pipes-home.component.ts
+++ src/app/pipes-module/pipes-home/pipes-home.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pipes-home',
   templateUrl: './pipes-home.component.html',
-  styleUrls: ['./pipes-home.component.css']
+  styleUrls: ['./pipes-home.component.css'],
 })
 export class PipesHomeComponent {
-
+  nameInput: string = '';
+  onNameChange(val: string) {
+    console.log(val);
+    this.nameInput = val;
+  }
 }
