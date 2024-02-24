2138e3465ac917d49fc5dbc064a4b44940cc6edf
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-24 14:50:19 +0330

    custom pipe with arguments

--- src/app/shared-module/custom-pipes/convert.pipe.ts
+++ src/app/shared-module/custom-pipes/convert.pipe.ts
@@ -4,8 +4,21 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'convert',
 })
 export class ConvertPipe implements PipeTransform {
-  transform(value: number, ...args: unknown[]): unknown {
-    console.log(value, 'value');
-    return value * 1.6;
+  transform(value: number, targetUnits: string): any {
+    //console.log(value, 'value');
+    //console.log(args, 'args');
+    if (!value) {
+      return '';
+    }
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.6;
+      case 'm':
+        return value * 1.6 * 1000;
+      case 'cm':
+        return value * 1.6 * 1000 * 100;
+      default:
+        throw new Error('not supported :' + targetUnits);
+    }
   }
 }
