568c994a426f71a27dcfefe8e3f92ef747cce39c
mohammad2rr <mohammad2rghdd@gmail.com>
2024-02-24 13:38:35 +0330

    added custom pipes folder in shared module

--- src/app/pipes-module/pipes-module.module.ts
+++ src/app/pipes-module/pipes-module.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PipesModuleRoutingModule } from './pipes-module-routing.module';
 import { PipesHomeComponent } from './pipes-home/pipes-home.component';
-import { ConvertPipe } from '../shared-module/convert.pipe';
+import { ConvertPipe } from '../shared-module/custom-pipes/convert.pipe';
 import { SharedModuleModule } from '../shared-module/shared-module.module';
 
 @NgModule({
--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomCardComponent } from './custom-card/custom-card.component';
-import { ConvertPipe } from './convert.pipe';
+import { ConvertPipe } from './custom-pipes/convert.pipe';
 
 @NgModule({
   declarations: [CustomCardComponent, ConvertPipe],
