2a901c4f54d7ce609803f832d37ff540767c6c34
mohammad2rr <mohammad2rghdd@gmail.com>
2024-03-10 16:56:49 +0330

    added styles and paginations

--- src/app/directives-module/directives-home/directives-home.component.ts
+++ src/app/directives-module/directives-home/directives-home.component.ts
@@ -14,20 +14,20 @@ export class DirectivesHomeComponent {
 
   images = [
     {
-      title: 'at the jungle',
+      title: 'at the jungle111',
       url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRIXl2wufWFoQzehBD7N3_TTfwYaEVTSvig&usqp=CAU',
     },
     {
-      title: 'at the jungle',
-      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRIXl2wufWFoQzehBD7N3_TTfwYaEVTSvig&usqp=CAU',
+      title: 'at the jungle222',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT79zq2eiP97ZgVZTnD64o9EsGu1Z-w6EZH1qsgNL1CjsLgnqj4PSx5KmKQZRlvFgY4wYA&usqp=CAU',
     },
     {
-      title: 'at the jungle',
-      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRIXl2wufWFoQzehBD7N3_TTfwYaEVTSvig&usqp=CAU',
+      title: 'at the jungle333',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXcCHWvrdHYgPrwBiQzoKfkqkZjABarjMVm0sd_iC45k9SWzvg9L3BNxN5VjwC-1ilO3E&usqp=CAU',
     },
     {
-      title: 'at the jungle',
-      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRIXl2wufWFoQzehBD7N3_TTfwYaEVTSvig&usqp=CAU',
+      title: 'at the jungle444',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS2CY1ZFdT0qqocCha0b3t5pjTZVaXIzFPx3rTLMpv8n2H5VXbytJETcRTu3pnXqgtzSc&usqp=CAU',
     },
   ];
 }
