94218b8d52e4f543541dc99b5b8d6cfdb229b0da
mohammad2rr <mohammad2rghdd@gmail.com>
2024-03-10 15:44:41 +0330

    ng-for and ng-class

--- src/app/directives-module/directives-home/directives-home.component.ts
+++ src/app/directives-module/directives-home/directives-home.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-directives-home',
   templateUrl: './directives-home.component.html',
-  styleUrls: ['./directives-home.component.css']
+  styleUrls: ['./directives-home.component.css'],
 })
 export class DirectivesHomeComponent {
+  currentPage: number = 0;
 
+  setIsCurrentClass(index: number) {
+    return index === this.currentPage ? 'is-current' : '';
+  }
+
+  images = [
+    {
+      title: 'at the jungle',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRIXl2wufWFoQzehBD7N3_TTfwYaEVTSvig&usqp=CAU',
+    },
+    {
+      title: 'at the jungle',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRIXl2wufWFoQzehBD7N3_TTfwYaEVTSvig&usqp=CAU',
+    },
+    {
+      title: 'at the jungle',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRIXl2wufWFoQzehBD7N3_TTfwYaEVTSvig&usqp=CAU',
+    },
+    {
+      title: 'at the jungle',
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRIXl2wufWFoQzehBD7N3_TTfwYaEVTSvig&usqp=CAU',
+    },
+  ];
 }
