fb2bfb360cd31d4a33e13823caf6e6487900c6df
mohammad2rr <mohammad2rghdd@gmail.com>
2024-04-25 10:25:16 +0330

    changed background of an element with renderer library called Renderer2

--- src/app/shared-module/directives/class-dir.directive.ts
+++ src/app/shared-module/directives/class-dir.directive.ts
@@ -1,14 +1,20 @@
-import { Directive } from '@angular/core';
-import { ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appClassDir]',
 })
 export class ClassDirDirective {
-  constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
     // console.log('call me');
     // console.log(this.elementRef, 'this.elementRef');
-    this.elementRef.nativeElement.style.backgroundColor = 'blue';
-    this.elementRef.nativeElement.style.color = 'gold';
+    // this.elementRef.nativeElement.style.backgroundColor = 'blue';
+    // this.elementRef.nativeElement.style.color = 'gold';
+    //.....................................................................
+    // using renderer library for changing background insted of element ref
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'green'
+    );
   }
 }
