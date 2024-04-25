3076deba2a1e3af0680af36b15cb2177547ca5f7
mohammad2rr <mohammad2rghdd@gmail.com>
2024-04-25 11:45:25 +0330

    added another directive and canged bgcolor in parent component with directive dinamically with setter

--- src/app/shared-module/directives/class-dir2.directive.ts
+++ src/app/shared-module/directives/class-dir2.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appClassDir2]'
+  selector: '[appClassDir2]',
 })
 export class ClassDir2Directive {
-
-  constructor() { }
-
+  // @Input() backgroundColor = 'pink';
+  // اگر به این صورت بنویسیم دفه اول که appClassDir2 توی پرنت صدا زده میشود رنگ صورتی داده میشود و بعد که سراغ  background میرود توی پرنت یک رنگ دیگری را میفرستیم اما دیگر متد سازنده صدا زده نمیشود .
+  @Input() set backgroundColor(color: string) {
+    this.elementRef.nativeElement.style.backgroundColor = color;
+  }
+  constructor(private elementRef: ElementRef) {}
 }
