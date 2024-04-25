0d92e48610cddd0ca7050e4c9bd7a77010af9ffe
mohammad2rr <mohammad2rghdd@gmail.com>
2024-04-25 10:19:19 +0330

    changed background of an element with directive

--- src/app/shared-module/directives/class-dir.directive.ts
+++ src/app/shared-module/directives/class-dir.directive.ts
@@ -1,10 +1,14 @@
 import { Directive } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appClassDir]',
 })
 export class ClassDirDirective {
-  constructor() {
-    console.log('call me');
+  constructor(private elementRef: ElementRef) {
+    // console.log('call me');
+    // console.log(this.elementRef, 'this.elementRef');
+    this.elementRef.nativeElement.style.backgroundColor = 'blue';
+    this.elementRef.nativeElement.style.color = 'gold';
   }
 }
