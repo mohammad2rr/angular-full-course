9cf13f34a85428628ade47e632e4e87b86479db6
mohammad2rr <mohammad2rghdd@gmail.com>
2024-04-25 11:05:36 +0330

    added another directive to show input and output functionality

---
+++ src/app/shared-module/directives/class-dir2.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appClassDir2]'
+})
+export class ClassDir2Directive {
+
+  constructor() { }
+
+}
--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { CustomCardComponent } from './custom-card/custom-card.component';
 import { ConvertPipe } from './custom-pipes/convert.pipe';
 import { ClassDirDirective } from './directives/class-dir.directive';
+import { ClassDir2Directive } from './directives/class-dir2.directive';
 
 @NgModule({
-  declarations: [CustomCardComponent, ConvertPipe, ClassDirDirective],
+  declarations: [
+    CustomCardComponent,
+    ConvertPipe,
+    ClassDirDirective,
+    ClassDir2Directive,
+  ],
   imports: [CommonModule],
-  exports: [CustomCardComponent, ConvertPipe, ClassDirDirective],
+  exports: [
+    CustomCardComponent,
+    ConvertPipe,
+    ClassDirDirective,
+    ClassDir2Directive,
+  ],
 })
 export class SharedModuleModule {}
