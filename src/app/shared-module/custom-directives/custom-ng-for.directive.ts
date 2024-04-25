4e802f3c2678110da7ccc261289d48a5ace9eaa2
mohammad2rr <mohammad2rghdd@gmail.com>
2024-04-25 14:39:48 +0330

    added custom ng for

--- src/app/shared-module/custom-directives/custom-ng-for.directive.ts
+++ src/app/shared-module/custom-directives/custom-ng-for.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: '[appCustomNgFor]'
+  selector: '[appCustomNgFor]',
 })
 export class CustomNgForDirective {
-
-  constructor() { }
-
+  @Input('appCustomNgFor') set render(time: number) {
+    for (let i = 0; i < time; i++) {
+      this.vcRef.createEmbeddedView(this.tempRef, {
+        index: i,
+        color: 'red',
+      });
+    }
+  }
+  constructor(
+    private tempRef: TemplateRef<any>,
+    private vcRef: ViewContainerRef
+  ) {}
 }
