30fcde1a2ff3e5ed9baabf53845169f4d58ecff5
mohammad2rr <mohammad2rghdd@gmail.com>
2024-05-08 23:52:23 +0330

    added accordion

--- src/app/semantic-ui-module/mods/accordion/accordion.component.ts
+++ src/app/semantic-ui-module/mods/accordion/accordion.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.css']
+  styleUrls: ['./accordion.component.css'],
 })
-export class AccordionComponent {
+export class AccordionComponent implements OnInit {
+  @Input() items = [];
+  openIndexItem = 0;
 
+  constructor() {}
+
+  onChangeShowItem(index: number) {
+    if (index === this.openIndexItem) {
+      this.openIndexItem = -1;
+    } else {
+      this.openIndexItem = index;
+    }
+  }
+
+  ngOnInit(): void {}
 }
