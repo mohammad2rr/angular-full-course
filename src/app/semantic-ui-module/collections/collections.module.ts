a2b1992977d73971fea645fd995f4eff12cc8691
mohammad2rr <mohammad2rghdd@gmail.com>
2024-05-08 12:22:49 +0330

    added needed components in collections module

--- src/app/semantic-ui-module/collections/collections.module.ts
+++ src/app/semantic-ui-module/collections/collections.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
+import { TableComponent } from './table/table.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 
 @NgModule({
   declarations: [
-    CollectionsHomeComponent
+    CollectionsHomeComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent,
+    TableComponent,
+    TabsComponent
   ],
   imports: [
     CommonModule,
